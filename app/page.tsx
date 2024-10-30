22aee12a0018468623b2cd01c1f75c9bb48d2521
Squeezr <samxiezx1115@gmail.com>
2024-10-30 11:20:11 -0400

    main: clicking mechanism in place

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import Head from 'next/head';
+import Image from "next/image";
 
 export default function Home() {
+
+  function weatherUpdate(event: React.MouseEvent<HTMLDivElement>) {
+    event.preventDefault()
+    console.log("INFO: weatherUpdate function triggered.")
+  }
+
   return (
     <div className="">
-      <main className="bg-blue-200 h-screen">
+      <main className="bg-blue-100 h-screen">
         <div className="flex justify-center md:justify-end ">
           <div className="w-36 border-8 border-gray-600 mt-5 md:mr-20 text-center">
             <p>Switch Toggle</p>
@@ -11,13 +20,22 @@ export default function Home() {
         </div>
 
         {/* Application Name */}
-        <div className="text-center m-5 text-xl font-bold">
+        <div className="text-center m-5 text-2xl font-bold">
           <h1>The Weather App</h1>
         </div>
 
         {/* Input Box */}
-        <div className="w-60 mx-auto flex-roq items-center">
-          <input className="border-8 border-gray-600 w-60 text-center rounded-lg"></input>
+        <div className="w-80 ml-auto mr-auto flex items-center">
+          <input placeholder="Name of the city" className="w-80 border-8 border-gray-600 text-center rounded-lg text-xl"></input>
+          <div className="ml-2" onClick={weatherUpdate}>
+            <Image
+              className="cursor-pointer"
+              src="/search.png" // add another search icon in white
+              alt="search icon logomark"
+              width={40}
+              height={40}
+            />
+          </div>
         </div>
       </main>
     </div>
